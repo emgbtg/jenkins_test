d4a013b4ef33cfcd4a46b82f93b1dc348b8faed7
Eric Gilbert <egilbert@ouiitercgilbert.urbanout.com>
2018-11-05 16:16:27 -0500

    trying to get groovy file working

--- AppPipeLineSteps.groovy
+++ AppPipeLineSteps.groovy
@@ -2,7 +2,7 @@
 def testFunc() {
     echo "running fastlane"
     sh "pwd"
-    sh "cd jenkins_test/jenkins_test_ios"
+    sh "cd /jenkins_test_ios"
     sh "fastlane generate_ipa_develop"
 }
 return this
